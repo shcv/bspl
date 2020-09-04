b72a0d7f1cf6a44223462f9ae0f678c119139171
Samuel Christie <shcv@sdf.org>
2020-09-04 11:41:28 -0400

    Add requirements.txt and setup.py

---
+++ setup.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+# Allow trove classifiers in previous python versions
+from sys import version
+if version < '2.2.3':
+    from distutils.dist import DistributionMetadata
+    DistributionMetadata.classifiers = None
+    DistributionMetadata.download_url = None
+
+from bungie import __version__ as version
+
+
+def requireModules(moduleNames=None):
+    import re
+    if moduleNames is None:
+        moduleNames = []
+    else:
+        moduleNames = moduleNames
+
+    commentPattern = re.compile(r'^\w*?#')
+    moduleNames.extend(
+        filter(lambda line: not commentPattern.match(line),
+               open('requirements.txt').readlines()))
+
+    return moduleNames
+
+
+# entry_points = {
+#     'console_scripts': ['bspl = protocheck.main:main'],
+# }
+
+setup(
+    name='bungie',
+    version=version,
+
+    author='Samuel Christie',
+    author_email='schrist@ncsu.edu',
+
+    description='Agent implementation framework',
+    long_description=open('README.org').read(),
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers'
+    ],
+
+    entry_points=entry_points,
+
+    install_requires=requireModules(),
+
+    packages=['bungie'],
+
+    test_suite='bungie'
+)
