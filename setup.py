02ee66e6b233a38ad5d8124bf8df820b224a89e6
Samuel Christie <shcv@sdf.org>
2020-09-09 17:08:49 -0400

    Fix packaging

--- setup.py
+++ setup.py
@@ -1,31 +1,5 @@
 #!/usr/bin/env python
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
-
-# Allow trove classifiers in previous python versions
-from sys import version
-if version < '2.2.3':
-    from distutils.dist import DistributionMetadata
-    DistributionMetadata.classifiers = None
-    DistributionMetadata.download_url = None
-
-
-def requireModules(moduleNames=None):
-    import re
-    if moduleNames is None:
-        moduleNames = []
-    else:
-        moduleNames = moduleNames
-
-    commentPattern = re.compile(r'^\w*?#')
-    moduleNames.extend(
-        filter(lambda line: not commentPattern.match(line),
-               open('requirements.txt').readlines()))
-
-    return moduleNames
-
+from setuptools import setup, find_packages
 
 # entry_points = {
 #     'console_scripts': ['bspl = protocheck.main:main'],
@@ -47,9 +21,15 @@ setup(
 
     # entry_points=entry_points,
 
-    install_requires=requireModules(),
+    install_requires=[
+        'protocheck @ git+https://gitlab.com/masr/protocheck.git',
+        'cronus @ git+https://github.com/priestoferis/cronus.git',
+        'python-crontab',
+        'croniter',
+        'pyyaml'
+    ],
 
-    packages=['bungie'],
+    packages=find_packages(),
 
     test_suite='bungie'
 )
