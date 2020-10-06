a8e34326b282bec6b67cc54bc762abd71620d345
Samuel Christie <shcv@sdf.org>
2020-10-06 11:59:57 -0400

    Add statistics module

---
+++ tests/test_statistics.py
@@ -0,0 +1,25 @@
+from bungie.statistics import *
+
+
+def test_net_usage():
+    net = net_usage('')
+    print(net)
+    assert net
+
+
+def test_cpu_usage():
+    cpu = cpu_usage()
+    print(cpu)
+    assert cpu
+
+
+def test_mem_usage():
+    mem = mem_usage()
+    print(mem)
+    assert mem
+
+
+def test_stats():
+    s = stats()
+    print(s)
+    assert s
