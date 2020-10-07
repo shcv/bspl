196a8594ca17d2aca872f7dbb7f1fa0338cd9772
Samuel Christie <shcv@sdf.org>
2020-10-07 13:07:36 -0400

    Add support for enactments

--- bungie/history.py
+++ bungie/history.py
@@ -2,6 +2,36 @@ import logging
 logger = logging.getLogger('bungie')
 
 
+class Enactment:
+    def __init__(self, parent=None):
+        self.subenactments = {}
+        self._bindings = {}
+        self._messages = []
+        self.parent = parent
+
+    def add(self, message):
+        self._bindings.update(message.payload)
+        self._messages.append(message)
+
+    @property
+    def bindings(self):
+        # may not be efficient enough, since it collects all of the
+        # bindings every time it's accessed
+        if self.parent:
+            return {**self.parent.bindings, **self._bindings}
+        else:
+            return self._bindings
+
+    @property
+    def messages(self):
+        if self.parent:
+            yield from self.parent.messages
+        yield from self._messages
+
+    def __repr__(self):
+        return f"Enactment(bindings={self.bindings},messages={[m for m in self.messages]},subenactments={self.subenactments})"
+
+
 class History:
     def __init__(self):
         # message index
@@ -10,6 +40,9 @@ class History:
         # parameter indexes
         self.all_bindings = {}
         self.bindings = {}
+
+        # recursive (key -> value -> enactment -> subkey -> value -> subenactment...)
+        self.enactments = {}
 
     def check_integrity(self, message):
         """
@@ -82,37 +115,25 @@ class History:
             else:
                 self.all_bindings[p] = set([message.payload[p]])
 
-        # log message under each key
-        for k in message.schema.keys:
-            v = message.payload.get(k)
-            if v is not None and k in self.by_param:
-                if v in self.by_param[k]:
-                    d = self.by_param[k][v]
-                    d['all'].add(message)
-                    if message.schema in d:
-                        d[message.schema].add(message)
-                    else:
-                        d[message.schema] = {message}
-                else:
-                    self.by_param[k][v] = {
-                        'all': {message},
-                        message.schema: {message}
-                    }
-            else:
-                self.by_param[k] = {
-                    v: {'all': {message}, message.schema: {message}}}
-
-        # update bindings
-        if message.key in self.bindings:
-            bs = self.bindings[message.key]
-            for p in message.payload:
-                if p not in bs:
-                    bs[p] = message.payload[p]
-        else:
-            self.bindings[message.key] = message.payload.copy()
+        # log under the correct enactment
+        enactment = self.enactment(message)
+        enactment.add(message)
 
     def enactment(self, message):
-        enactment = {"bindings": self.bindings.get(message.key)}
+        enactments = self.enactments
+        enactment = None
+        for k in message.schema.keys:
+            # assume sequential hierarchy of key parameters
+            v = message.payload.get(k)
+            if k in enactments:
+                if v is not None and v in enactments[k]:
+                    enactment = enactments[k][v]
+                    enactments = enactment.subenactments
+                else:
+                    enactment = enactments[k][v] = Enactment(parent=enactment)
+            else:
+                enactments[k] = {}
+                enactment = enactments[k][v] = Enactment(parent=enactment)
         return enactment
 
     def duplicate(self, message):
@@ -142,12 +163,14 @@ class History:
                 match.acknowledged = True
 
     def fill(self, message):
-        enactment = self.enactment(message)
+        Enactment = self.enactment(message)
+        bindings = enactment.bindings
+
         for p in message.schema.parameters:
-            v = enactment['bindings'].get(p)
+            v = bindings.get(p)
             if v:
                 message.payload[p] = v
             else:
                 raise Exception(
-                    "Cannot complete message {} with enactment {}".format(message, enactment))
+                    f"Cannot complete message {message} with enactment {enactment}")
         return message
