4ee8f51f02393d10f3f9f640639792b157e3dd5b
Samuel Christie <shcv@sdf.org>
2020-12-16 13:58:27 +0000

    Create variants of prescription scenario

---
+++ scenarios/prescription/configuration_ack.py
@@ -0,0 +1,40 @@
+from protocheck import bspl
+
+prescription = bspl.load_file(
+    "prescription-ack.bspl").protocols['Prescription']
+
+Patient = prescription.roles['Patient']
+Doctor = prescription.roles['Doctor']
+Pharmacist = prescription.roles['Pharmacist']
+
+Complain = prescription.messages['Complain']
+Repeat = prescription.messages['Repeat']
+Confirm = prescription.messages['Confirm']
+Prescribe = prescription.messages['Prescribe']
+Filled = prescription.messages['Filled']
+
+Map = {
+    "forwards": {
+        Complain: (Repeat, 'rID')
+    },
+    "acknowledgments": {
+        Complain: (Confirm, 'ackID'),
+        Repeat: (Confirm, 'ackID')
+    }
+}
+
+with open('/proc/self/cgroup', 'r') as cgroups:
+    in_docker = 'docker' in cgroups.read()
+
+if in_docker:
+    config = {
+        prescription.roles['Patient']: ('patient', 8000),
+        prescription.roles['Doctor']: ('doctor', 8000),
+        prescription.roles['Pharmacist']: ('pharmacist', 8000),
+    }
+else:
+    config = {
+        prescription.roles['Patient']: ('0.0.0.0', 8000),
+        prescription.roles['Doctor']: ('0.0.0.0', 8001),
+        prescription.roles['Pharmacist']: ('0.0.0.0', 8002),
+    }
