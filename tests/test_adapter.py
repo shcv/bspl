6b73b11c69b3641408465ca23563d576a1012b2c
Samuel Christie <shcv@sdf.org>
2021-02-11 17:40:47 -0500

    Fix adapter tests; add stop code to Emitter and Receiver

--- tests/test_adapter.py
+++ tests/test_adapter.py
@@ -1,8 +1,12 @@
 import asyncio
+import logging
+import pytest
 from protocheck import bspl
 from bungie.adapter import Adapter, Message
+from bungie.emitter import Emitter
 
-specification = bspl.parse("""
+specification = bspl.parse(
+    """
 Order {
   roles C, S // Customer, Seller
   parameters out item key, out done
@@ -18,38 +22,47 @@ With-Reject {
   Order(C, S, out item, out done)
   S -> C: Reject[in item, out done]
 }
-""")
+"""
+)
 
-with_reject = specification.protocols['With-Reject']
+with_reject = specification.protocols["With-Reject"]
 
 config = {
-    with_reject.roles['C']: ('localhost', 8001),
-    with_reject.roles['S']: ('localhost', 8002)
+    with_reject.roles["C"]: ("localhost", 8001),
+    with_reject.roles["S"]: ("localhost", 8002),
 }
 
-
-def test_receive_process():
-    a = Adapter(with_reject.roles['S'], with_reject, config)
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(a.task())
-    loop.run_until_complete(a.process_receive({"item": "ball"}))
-    print(a.history.all_bindings)
+logger = logging.getLogger("bungie")
+logger.setLevel(logging.DEBUG)
 
 
-def test_send_process():
-    a = Adapter(with_reject.roles['C'], with_reject, config)
-    m = Message(with_reject.messages['Buy'], {"item": "ball"})
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(a.task())
-    loop.run_until_complete(a.process_send(m))
+@pytest.mark.asyncio
+async def test_receive_process():
+    a = Adapter(with_reject.roles["S"], with_reject, config)
+    await a.task()
+    await a.receive({"item": "ball"})
+    await a.stop()
+
+    print(f"all bindings: {a.history.all_bindings}")
 
 
-def test_load_policies():
-    a = Adapter(with_reject.roles['C'], with_reject, config)
-    policies = """
-    C:
-      every 1s:
-        - resend Buy until received Deliver
-    """
-    a.load_policies(policies)
-    assert(a.schedulers)
+@pytest.mark.asyncio
+async def test_send_process():
+    a = Adapter(with_reject.roles["C"], with_reject, config, emitter=Emitter())
+    m = Message(with_reject.messages["Buy"], {"item": "ball"})
+    await a.task()
+    await a.process_send(m)
+    await a.stop()
+
+
+@pytest.mark.asyncio
+async def test_match():
+    """Test that the schema.match(**params) method works"""
+    # create adapter and inject methods
+    a = Adapter(with_reject.roles["S"], with_reject, config)
+    await a.task()
+    # make sure there's a Buy in the history
+    await a.receive({"item": "ball"})
+    ms = with_reject.messages["Deliver"].match(item="ball")
+    assert len(ms) > 0
+    await a.stop()
