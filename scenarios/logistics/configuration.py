05ab2e118e3826f78075614ad055d2d7dd0fcb11
Samuel Christie <shcv@sdf.org>
2021-02-11 17:38:35 -0500

    Use protocol modules in logistics scenario

--- scenarios/logistics/configuration.py
+++ scenarios/logistics/configuration.py
@@ -1,22 +1,23 @@
 from protocheck import bspl
 
-spec = bspl.load_file("logistics.bspl")
-logistics = spec.protocols["Logistics"]
+logistics = bspl.load_file("logistics.bspl").export("Logistics")
+from Logistics import Merchant, Wrapper, Labeler, Packer
 
 with open("/proc/self/cgroup", "r") as cgroups:
+
     in_docker = "docker" in cgroups.read()
 
 if in_docker:
     config = {
-        logistics.roles["Merchant"]: ("merchant", 8000),
-        logistics.roles["Wrapper"]: ("wrapper", 8001),
-        logistics.roles["Labeler"]: ("labeler", 8002),
-        logistics.roles["Packer"]: ("packer", 8003),
+        Merchant: ("merchant", 8000),
+        Wrapper: ("wrapper", 8001),
+        Labeler: ("labeler", 8002),
+        Packer: ("packer", 8003),
     }
 else:
     config = {
-        logistics.roles["Merchant"]: ("0.0.0.0", 8000),
-        logistics.roles["Wrapper"]: ("0.0.0.0", 8001),
-        logistics.roles["Labeler"]: ("0.0.0.0", 8002),
-        logistics.roles["Packer"]: ("0.0.0.0", 8003),
+        Merchant: ("0.0.0.0", 8000),
+        Wrapper: ("0.0.0.0", 8001),
+        Labeler: ("0.0.0.0", 8002),
+        Packer: ("0.0.0.0", 8003),
     }
--- scenarios/logistics/packer.py
+++ scenarios/logistics/packer.py
@@ -3,54 +3,22 @@ from bungie import Adapter, Resend
 from configuration import config, logistics
 from bungie.performance import perf_logger
 
-Labeled = logistics.messages["Labeled"]
-Wrapped = logistics.messages["Wrapped"]
-Packed = logistics.messages["Packed"]
+from Logistics import Packer, Labeled, Wrapped, Packed
 
-adapter = Adapter(logistics.roles["Packer"], logistics, config)
+adapter = Adapter(Packer, logistics, config)
 
 logger = logging.getLogger("bungie")
 # logger.setLevel(logging.DEBUG)
 
 
-@adapter.reaction(Labeled)
-async def labeled(message):
-    orderID = message.payload["orderID"]
-
-    unpacked = [
-        m
-        for m in adapter.history.by_param["orderID"][orderID].get(Wrapped, [])
-        if not m.meta.get("packed")
-    ]
-
-    for m in unpacked:
-        payload = {
-            "orderID": orderID,
-            "itemID": m.payload["itemID"],
-            "wrapping": m.payload["wrapping"],
-            "label": message.payload["label"],
-            "status": "packed",
-        }
-        adapter.send(payload, Packed)
-        m.packed = True
+async def pack(message):
+    for msg in Packed.match(**message.payload):
+        msg.status = "packed"
+        msg.send()
 
 
-@adapter.reaction(Wrapped)
-async def wrapped(message):
-    orderID = message.payload["orderID"]
-    label = adapter.history.bindings.get(f"orderID:{orderID}", {}).get("label")
-
-    if label:
-        payload = {
-            "orderID": orderID,
-            "itemID": message.payload["itemID"],
-            "wrapping": message.payload["wrapping"],
-            "label": label,
-            "status": "packed",
-        }
-        adapter.send(payload, Packed)
-        message.meta["packed"] = True
-
+adapter.register_reactor(Labeled, pack)
+adapter.register_reactor(Wrapped, pack)
 
 if __name__ == "__main__":
     logger.info("Starting Packer...")
