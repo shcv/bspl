5841b35df7d52e32d810a19bb13c2773530774ce
Samuel Christie <shcv@sdf.org>
2021-02-18 05:19:25 -0500

    Create simple script to start all of the agents

---
+++ scenarios/logistics/start.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+python packer.py &
+PACKER=$!
+
+# python wrapper.py &
+# WRAPPER=$!
+
+python labeler.py &
+LABELER=$!
+
+sleep 1
+python merchant.py &
+MERCHANT=$!
+
+read -n1 -rsp $'Press any key to stop...\n'
+
+kill $PACKER $WRAPPER $LABELER $MERCHANT
+
+
+
