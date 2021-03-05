015b3e0de83cbd6bff32d97ecba849c9003e0c5a
Samuel Christie <shcv@sdf.org>
2021-03-05 14:50:44 -0500

    Update packaging

--- setup.py
+++ setup.py
@@ -1,30 +1,4 @@
 #!/usr/bin/env python
-from setuptools import setup, find_packages
+from setuptools import setup
 
-# entry_points = {
-#     'console_scripts': ['bspl = protocheck.main:main'],
-# }
-
-setup(
-    name="bungie",
-    version="0.0.0",
-    author="Samuel Christie",
-    author_email="schrist@ncsu.edu",
-    description="Agent implementation framework",
-    long_description=open("README.org").read(),
-    classifiers=[
-        "Development Status :: 2 - Pre-Alpha",
-        "Intended Audience :: Developers",
-    ],
-    # entry_points=entry_points,
-    install_requires=[
-        "protocheck @ git+https://gitlab.com/masr/protocheck.git",
-        "aiocron",
-        "pyyaml",
-        "ijson",
-        "aiorun",
-        "uvloop",
-    ],
-    packages=find_packages(),
-    test_suite="bungie",
-)
+setup()
