8242825cf0f613d71aea4fc408bb98c84ecfdc71
Samuel Christie <shcv@sdf.org>
2021-03-29 16:46:18 -0400

    Move import of build inside run() method
    Should delay import of module with dependencies until after those dependencies
    are installed

--- setup.py
+++ setup.py
@@ -1,15 +1,14 @@
 #!/usr/bin/env python
 from setuptools import setup
 from setuptools.command.build_py import build_py
-
 import sys
-
-sys.path.append("./protocheck/bspl")
-from build import build_parser, save_parser
 
 
 class Build(build_py):
     def run(self):
+        sys.path.append("./")
+        from protocheck.bspl.build import build_parser, save_parser
+
         model = build_parser()
         save_parser(model)
         super(Build, self).run()
