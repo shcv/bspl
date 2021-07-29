7d0ff038bf88fad2c21a11570fd7b211ec855ac3
Samuel Christie <shcv@sdf.org>
2021-07-29 16:08:30 -0400

    Rewrite Wrapper as reaction model demo

--- scenarios/logistics/wrapper.py
+++ scenarios/logistics/wrapper.py
@@ -3,25 +3,22 @@ from configuration import config, logistics, Wrapped, RequestWrapping
 import logging
 
 logger = logging.getLogger("wrapper")
-# logging.getLogger('bungie').setLevel(logging.DEBUG)
+# logging.getLogger("bungie").setLevel(logging.DEBUG)
 
 adapter = Adapter(logistics.roles["Wrapper"], logistics, config)
 
 
-@adapter.enabled(Wrapped)
+@adapter.reaction(RequestWrapping)
 async def wrap(msg):
-    item = msg.payload["item"]
-    msg.bind(wrapping="bubblewrap" if item in ["plate", "glass"] else "paper")
+    adapter.send(
+        Wrapped(
+            wrapping="bubblewrap" if msg["item"] in ["plate", "glass"] else "paper",
+            **msg.payload
+        )
+    )
     return msg
 
 
 if __name__ == "__main__":
     logger.info("Starting Wrapper...")
-
-    # acknowledge Complain
-    # adapter.add_policies(
-    #     Acknowledge(RequestWrapping).Map(Map),
-    #     Acknowledge(RequestWrappingReminder).With(RequestWrappingAck, "ackID"),
-    # )
-
     adapter.start()
