fcc60af9948934f33bfddd7dd73a724cd9483971
Samuel Christie <shcv@sdf.org>
2021-10-26 16:45:18 -0400

    Remove docker support for logistics
    Simplifies configuration, and should improve compatibility with Windows

--- scenarios/logistics/configuration.py
+++ scenarios/logistics/configuration.py
@@ -11,21 +11,9 @@ from Logistics import (
     Packed,
 )
 
-with open("/proc/self/cgroup", "r") as cgroups:
-
-    in_docker = "docker" in cgroups.read()
-
-if in_docker:
-    config = {
-        Merchant: ("merchant", 8000),
-        Wrapper: ("wrapper", 8001),
-        Labeler: ("labeler", 8002),
-        Packer: ("packer", 8003),
-    }
-else:
-    config = {
-        Merchant: ("0.0.0.0", 8000),
-        Wrapper: ("0.0.0.0", 8001),
-        Labeler: ("0.0.0.0", 8002),
-        Packer: ("0.0.0.0", 8003),
-    }
+config = {
+    Merchant: ("0.0.0.0", 8000),
+    Wrapper: ("0.0.0.0", 8001),
+    Labeler: ("0.0.0.0", 8002),
+    Packer: ("0.0.0.0", 8003),
+}
