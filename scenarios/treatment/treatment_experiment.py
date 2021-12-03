0122a1757e47bd797ed38e4d57e43545c8e8a0c2
Samuel Christie <shcv@sdf.org>
2021-12-03 00:12:29 -0500

    Clean up the experiment, and generate figures

--- scenarios/treatment/treatment_experiment.py
+++ scenarios/treatment/treatment_experiment.py
@@ -20,34 +20,28 @@ from doctor import adapter as doctor
 from pharmacist import adapter as pharmacist
 from configuration import *
 
+idx = pd.IndexSlice
+
 experiment = Experiment(
     name="treatment",
     parameters={
-        "iteration": range(5),
         "max-time": [30],
         "complaints": [1000],
+        "lossy": [
+            [],
+            ["patient"],
+            ["doctor"],
+            ["pharmacist"],
+            ["doctor", "pharmacist"],
+            ["patient", "doctor", "pharmacist"],
+        ],
+        "loss-rate": [0.01, 0.05, 0.25],
         "recovery": (
             "none",
             "retry",
             "checkpoint",
         ),
-        "loss": [
-            # patient, doctor, pharmacist
-            # baseline
-            (0, 0, 0),
-            # only patient
-            (0.01, 0, 0),
-            (0.1, 0, 0),
-            (0.5, 0, 0),
-            # only doctor
-            (0, 0.01, 0),
-            (0, 0.1, 0),
-            (0, 0.5, 0),
-            # all three
-            (0.01, 0.01, 0.01),
-            (0.1, 0.1, 0.1),
-            (0.5, 0.5, 0.5),
-        ],
+        "iteration": range(5),
     },
 )
 
@@ -55,7 +49,10 @@ experiment = Experiment(
 @experiment.preprocess
 def listify(exp):
     for k in exp.keys:
-        k["loss"] = tuple(k["loss"])
+        k.pop("max-time")
+        k.pop("complaints")
+        k["lossy"] = tuple(k["lossy"])
+        k["loss-rate"] = float(k["loss-rate"])
 
 
 def split_losses(exp):
@@ -74,7 +71,8 @@ def patient_proc(parameters, queue):
     # patient.logger.setLevel(logging.DEBUG)
     # mandrake.scheduler.logger.setLevel(logging.DEBUG)
     # mandrake.policies.logger.setLevel(logging.DEBUG)
-    patient.emitter.loss = parameters["loss"][0]
+    if "patient" in parameters["lossy"]:
+        patient.emitter.loss = parameters["loss-rate"]
 
     statistics.stats["completed"] = 0
 
@@ -162,7 +160,8 @@ def patient_proc(parameters, queue):
 
 def doctor_proc(parameters):
     # doctor.logger.setLevel(logging.DEBUG)
-    doctor.emitter.loss = parameters["loss"][1]
+    if "doctor" in parameters["lossy"]:
+        doctor.emitter.loss = parameters["loss-rate"]
     if parameters["recovery"] == "retry":
         # add retry policies
         doctor.add_policies(
@@ -184,7 +183,8 @@ def doctor_proc(parameters):
 
 def pharmacist_proc(parameters):
     # pharmacist.logger.setLevel(logging.DEBUG)
-    pharmacist.emitter.loss = parameters["loss"][2]
+    if "pharmacist" in parameters["lossy"]:
+        pharmacist.emitter.loss = parameters["loss-rate"]
     if parameters["recovery"] == "retry":
         # add retry policies
         pharmacist.add_policies(
@@ -237,8 +237,166 @@ def main(**kwargs):
 def post(df):
     df["duration"] = pd.to_numeric(df["duration"])
     df["rate"] = df["completed"] / df["duration"]
+    df = df.drop(columns=["duration"])
 
     return df
+
+
+@experiment.postprocess
+def results(df):
+    avg = df.groupby(["lossy", "loss-rate", "recovery"]).mean()
+    std = df.groupby(["lossy", "loss-rate", "recovery"]).std()
+    columns = [c for c in avg.columns]
+    print(columns)
+    for col in columns:
+        avg[col + "-std"] = std[col]
+    experiment.columns = columns
+    return avg
+
+
+@experiment.command("write-data")
+def write_data():
+    for case in experiment.parameters["lossy"]:
+        casename = "_".join(case)
+        for col in experiment.columns:
+            for recovery in ["none", "retry", "checkpoint"]:
+                filename = "_".join([col] + case + [recovery]) + ".dat"
+                print(f"Writing data to {filename} with fields: loss, {col}, err")
+                experiment.frame.loc[tuple(case)].xs(recovery, level="recovery")[
+                    [col, col + "-std"]
+                ].to_string(
+                    "data/" + filename, sparsify=False, header=False, index_names=False
+                )
+    return experiment
+
+
+class Figure:
+    def __init__(self, caption, label):
+        self.subfigures = []
+        self.caption = caption
+        self.label = label
+
+    def add(self, sub):
+        self.subfigures.append(sub)
+
+    def __str__(self):
+        nl = "\n"
+        return f"""\
+\\begin{{figure}}[ht]
+\\centering
+{nl.join(str(s) for s in self.subfigures)}
+\\caption{{{self.caption}}}
+\\label{{fig:{self.label}}}
+\\end{{figure}}
+
+"""
+
+
+class Subfigure:
+    def __init__(self, context, case, caption, xlabel=None, ylabel=None, legend=None):
+        self.case = case
+        self.caption = caption
+        self.xlabel = f"xlabel = {{loss rate}}," if xlabel else ""
+        self.ylabel = f"ylabel = {{{ylabel}}}," if ylabel else ""
+        self.plots = []
+        self.name = context + "-" + "-".join(case or ["no-loss"]) + "-subfig.tex"
+        self.legend = f"\\legend{{{','.join(legend)}}}" if legend else ""
+
+    def add(self, *plots):
+        self.plots.extend(plots)
+
+    def write(self, path):
+        with open(path + "/" + self.name, "w") as f:
+            f.write(
+                f"""\
+\\begin{{tikzpicture}}
+\\begin{{axis}}[{self.xlabel} {self.ylabel}
+  width = 4cm, height = 4cm,
+  legend style = {{at={{(1,1.1)}},anchor=south east}}
+]
+{"".join(str(p) for p in self.plots)}\
+{{{self.legend}}}
+\\end{{axis}}
+\\end{{tikzpicture}}
+"""
+            )
+
+    def __str__(self):
+        inclusion = f"\\subcaptionbox{{{self.caption}}}[0.3\\textwidth]{{\includegraphics{{JAAMAS/figs/{self.name}}}}}"
+        if self.ylabel:
+            inclusion += "\n\hspace{.5em}"
+        return inclusion
+
+
+def plot(filename):
+    return f"\\addplot table[x index=0,y index=1,y error index=2, header=false] {{JAAMAS/data/{filename}}};\n"
+
+
+case_map = {
+    tuple(): {"caption": "No Loss", "ylabel": True},
+    ("patient",): {"caption": "Patient Only"},
+    ("doctor",): {"caption": "Doctor Only", "legend": True},
+    ("pharmacist",): {"caption": "Pharmacist Only", "ylabel": True},
+    ("doctor", "pharmacist"): {"caption": "Docter + Pharmacist"},
+    ("patient", "doctor", "pharmacist"): {
+        "caption": "All",
+    },
+}
+
+captions = {
+    "completed": """Total enactments completed.
+Each subfigure represents a different loss configuration, with the lines representing the recovery policies.
+In this figure, a line is also given for the absence of a recovery policy, to show the cumulative effect of the loss rate.
+Subfigure (a) has no loss, (b-d) have one lossy agent, (e) has both doctor and pharmacist lossy, and in (f) all agents are equally lossy.
+The Y-axes show the number of enactments completed by the timeout, which is set to be 30 seconds.
+The X-axes are the three different loss rates tested: 0.01, 0.05, and 0.25.""",
+    "emissions": """Messages emitted.
+Each subfigure represents a different loss configuration, with the lines representing the two recovery policies.
+Subfigure (a) has no loss, (b-d) have one lossy agent, (e) has both doctor and pharmacist lossy, and in (f) all agents are equally lossy.
+The Y-axes show the number of messages emitted by \\rname{Patient}.
+The X-axes are the three different loss rates tested: 0.01, 0.05, and 0.25.""",
+    "packets": """Total packets sent.
+Each subfigure represents a different loss configuration, with the lines representing the two recovery policies.
+Subfigure (a) has no loss, (b-d) have one lossy agent, (e) has both doctor and pharmacist lossy, and in (f) all agents are equally lossy.
+The Y-axes show the number of UDP packets sent by \\rname{Patient}.
+The X-axes are the three different loss rates tested: 0.01, 0.05, and 0.25.""",
+    "rate": """Rate of completion (enactments/second).
+Each subfigure represents a different loss configuration, with the lines representing the two recovery policies.
+Subfigure (a) has no loss, (b-d) have one lossy agent, (e) has both doctor and pharmacist lossy, and in (f) all agents are equally lossy.
+The Y-axes show the number of enactments completed (by observing \mname{FilledRx} or one of its reminders) divided by the duration of the iteration.
+The X-axes are the three different loss rates tested: 0.01, 0.05, and 0.25.""",
+}
+
+
+@experiment.command("figure")
+def figures():
+    columns = [c for c in experiment.columns]
+    with open(f"figs/figures.tex", "w") as f:
+        for col in columns:
+            fig = Figure(captions[col], col)
+            for case in experiment.parameters["lossy"]:
+                casename = "_".join(case)
+                ylabel = (
+                    col.capitalize() if case_map[tuple(case)].get("ylabel") else None
+                )
+                legend = ["None", "Retry", "Checkpoint"]
+                if col != "completed":
+                    legend = legend[1:]
+                subfig = Subfigure(
+                    col,
+                    tuple(case),
+                    case_map[tuple(case)]["caption"],
+                    True,
+                    ylabel,
+                    legend if case_map[tuple(case)].get("legend") else None,
+                )
+                for recovery in ["none", "retry", "checkpoint"]:
+                    if not (col != "completed" and recovery == "none"):
+                        filename = "_".join([col] + case + [recovery]) + ".dat"
+                        subfig.add(plot(filename))
+                subfig.write("figs")
+                fig.add(subfig)
+            f.write(str(fig))
 
 
 # run patient until max-time or number of complaints; return results from patient
