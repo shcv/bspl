d730125a6401c1b62de8a9130b44f089c0744c2e
Samuel Christie <shcv@sdf.org>
2022-07-28 15:41:20 -0400

    Update guix packaging to make a usable dev env
    - Remove unnecessary manifest.scm
    - Remove protocheck and bungie packages
    - Add python-bspl and python-bspl-dev packages
    - add .envrc to automatically setup dev env

--- guix/masr/packages.scm
+++ guix/masr/packages.scm
@@ -4,69 +4,63 @@
   #:use-module (masr packages python-xyz)
   #:use-module ((guix licenses) #:prefix license:)
   #:use-module (gnu packages)
+  #:use-module (gnu packages base)
   #:use-module (gnu packages check)
   #:use-module (gnu packages python-xyz)
   #:use-module (gnu packages python-web)
   #:use-module (guix packages)
+  #:use-module (guix gexp)
   #:use-module (guix git-download)
   #:use-module (guix build-system python)
   #:use-module (srfi srfi-1))
 
-(define-public protocheck
+(define-public python-bspl
   (package
-    (name "protocheck")
-    (version "0.3.0")
-    (source
-     (origin
-       (method git-fetch)
-       (uri (git-reference
-             (url "https://gitlab.com/masr/protocheck")
-             (commit "61623fa6821db7ffc12cd5d2aaba591ae2fc526")))
-       (sha256
-        (base32
-         "0x818sfcw49c6zfc42yrnin20q3fp5095aigmyzv3ybihs8qpsan"))))
-    (build-system python-build-system)
-    (propagated-inputs
-     `(("python-ttictoc" ,python-ttictoc)
-       ("python-simplejson" ,python-simplejson)
-       ("python-configargparse" ,python-configargparse)
-       ("python-boolexpr" ,python-boolexpr)
-       ("python-tatsu" ,python-tatsu)))
-    (native-inputs
-     `(("python-setuptools-git" ,python-setuptools-git)
-       ("python-pytest" ,python-pytest)))
-    (home-page "https://gitlab.com/masr/protocheck")
-    (synopsis "BSPL protocol tool suite")
-    (description "Protocheck is a tool suite for parsing, verifying, and working with BSPL protocol specifications.")
-    (license license:expat)))
-
-(define-public python-bungie
-  (package
-    (name "python-bungie")
+    (name "python-bspl")
     (version "0.0.0")
     (source
      (origin
        (method git-fetch)
        (uri (git-reference
-             (url "https://gitlab.com/masr/bungie")
+             (url "https://gitlab.com/masr/bspl")
              (commit "f853c145e9c425eb99eddc5a9559737376eb5e2")))
        (sha256
         (base32
          "1ir949z28ng4xll3pd0n3clizd5n378qa634vc8kvkxna4cghn70"))))
     (build-system python-build-system)
-    (native-inputs
-     `(("python-setuptools-git" ,python-setuptools-git)
-       ("python-pytest" ,python-pytest)))
     (propagated-inputs
-     `(("protocheck" ,protocheck)
-       ("python-tatsu" ,python-tatsu)
-       ("python-uvloop" ,python-uvloop)
-       ("python-aiorun" ,python-aiorun)
-       ("python-aiocron" ,python-aiocron)
-       ("python-ijson" ,python-ijson)
-       ("python-pyyaml" ,python-pyyaml)))
-    (home-page "https://gitlab.com/masr/bungie")
-    (synopsis "A python framework for implementing fault tolerant agents as microservices")
-    (description "Bungie is a simple framework for implementing agents as microservices, with additional support for fault-tolerance policies.")
+     (list
+      python-agentspeak
+      python-aiocron
+      python-aiorun
+      python-boolexpr
+      python-colorama
+      python-fire
+      python-ijson
+      python-pyyaml
+      python-simplejson
+      python-tatsu
+      python-ttictoc
+      python-uvloop))
+    (native-inputs
+     (list python-setuptools-git
+           python-pytest
+           python-pytest-asyncio))
+    (arguments
+     `(#:phases
+       (modify-phases %standard-phases
+         (replace 'check
+           (lambda _
+             (setenv "PYTHONPATH" "src")
+             (invoke "pytest"))))))
+    (home-page "https://gitlab.com/masr/bspl")
+    (synopsis "A python framework for implementing agents according to protocol specifications")
+    (description "This repository provides tools for working with the language, including a parser and verification tools (proving safety, liveness, etc.))))
+It also provides a library for implementing agents that can enact BSPL protocols.")
     (license license:expat)))
 
+(define-public python-bspl-dev
+  (package
+    (inherit python-bspl)
+    (name "python-bspl-dev")
+    (source (local-file (getcwd) #:recursive? #t))))
