7ed4460be862bc0879337e394ab3ae9a5d4aec7f
Samuel Christie <shcv@sdf.org>
2025-01-13 16:22:08 -0500

    Add 'unsafe' and 'nonlive' query generators

--- src/bspl/generators/mambo.py
+++ src/bspl/generators/mambo.py
@@ -1,8 +1,36 @@
 #!/usr/bin/env python3
 
 from bspl.parsers.bspl import load_file
-from bspl.verification.mambo import Query, Or, Not, And, occurs
+from bspl.verification.mambo import Occurs
 from itertools import combinations, chain
+
+
+def unsafe(protocol):
+    sources = {
+        p: {m.name for m in protocol.messages.values() if p in m.outs}
+        for p in protocol.parameters
+    }
+    conflicts = [sources[p] for p in sources if len(sources[p]) > 1]
+    if not conflicts:
+        return None
+    # safety = not ((m1 and m2) or (m3 and m4) ...) for all pairs of conflicting messages
+    # compute And(m1, m2) for all pairs of conflicting messages
+    pairs = list(
+        chain.from_iterable(combinations([Occurs(m) for m in c], 2) for c in conflicts)
+    )
+    # compute disjunction of all pairs
+    clause = pairs[0][0] & pairs[0][1]
+    for p in pairs[1:]:
+        clause = clause | (p[0] & p[1])
+    return clause
+
+
+def nonlive(protocol):
+    outs = list(protocol.outs)
+    expr = ~Occurs(outs[0])
+    for o in outs[1:]:
+        expr = expr | ~Occurs(o)
+    return expr
 
 
 class MamboGenerator:
@@ -14,26 +42,11 @@ class MamboGenerator:
             Ps = spec.protocols.values()
 
         for P in Ps:
-            sources = {
-                p: {m.name for m in P.messages.values() if p in m.outs}
-                for p in P.parameters
-            }
-            conflicts = [sources[p] for p in sources if len(sources[p]) > 1]
-            if not conflicts:
+            conjs = unsafe(P)
+            if conjs:
+                yield f"{P.name}: {conjs}"
+            else:
                 yield f"{P.name}: safe"
-                continue
-            # safety = not ((m1 and m2) or (m3 and m4) ...) for all pairs of conflicting messages
-            # compute And(m1, m2) for all pairs of conflicting messages
-            pairs = list(
-                chain.from_iterable(
-                    combinations([Query(occurs, m) for m in c], 2) for c in conflicts
-                )
-            )
-            # compute disjunction of all pairs
-            conjs = Query(And, pairs[0][0], pairs[0][1])
-            for p in pairs[1:]:
-                conjs = Query(Or, disjuncts, Query(And, p[0], p[1]))
-            yield f"{P.name}: {conjs}"
 
     def nonlive(self, path, protocol=None):
         """Protocols are non-live if any of the out parameters are unbound"""
@@ -44,8 +57,5 @@ class MamboGenerator:
             Ps = spec.protocols.values()
 
         for P in Ps:
-            outs = list(P.outs)
-            expr = Query(occurs, outs[0])
-            for o in outs[1:]:
-                expr = Query(Or, expr, Query(occurs, o))
-            yield f"{P.name}: {Query(Not, expr)}"
+            expr = nonlive(P)
+            yield f"{P.name}: {expr}"
