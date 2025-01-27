57fcb68ff9f51b64a10b359d82b5f5b9d0cffee7
Samuel Christie <shcv@sdf.org>
2025-01-27 15:32:08 -0500

    Merge mambo-new-props.py into mambo-props.py

--- experiments/mambo-props.py
+++ experiments/mambo-props.py
@@ -3,63 +3,105 @@
 import spearmint as sp
 from spearmint import Experiment
 import fire
+from pprint import pformat
 from bspl.parsers import bspl, precedence
-from bspl.verification.mambo import match_paths, QuerySemantics
+from bspl.verification.mambo import match_paths, QuerySemantics, Any
 from bspl.verification.paths import UoD
 import bspl.verification.paths as tango
 from bspl.generators.mambo import unsafe, nonlive
 
+from bspl.verification import paths as tango
+from bspl.verification import mambo
+
 Sale = bspl.load_file("samples/sale.bspl").protocols["Sale"]
 
 experiment = Experiment(
-    name="mambo",
+    name="mambo-props",
     parameters={
-        "query": {
-            "nonlive": nonlive(Sale),
-            "unsafe": unsafe(Sale),
-            "desired-end": "rescindAck ∨ reject ∨ transfer ∧ (refund ∨ deliver)",
-            "happy": "offer · accept · transfer · deliver",
-            "happy-2": "offer · accept · deliver · transfer",
-            "unhappy-1": "offer . accept . rescind . rescindAck",
-            "unhappy-2": "offer . reject",
-            "late-action": "no accept ∨ no rescind or accept · rescind",
-            "alternatives": "transfer . deliver or transfer . refund",
-            "priority": "no Buyer:rescind ∨ Buyer:pay ∨ rescindAck",
-            "compensation": "(no transfer ∨ deliver ∨ refund) ∧ (no refund ∨ transfer) ∧ (no refund ∨ no deliver)",
-            "nonexclusive": "reject and deliver",
-            "delegation": "pay | transfer | no pay . transfer",
-            "delegation-2": "(no pay or transfer) and (no transfer or pay . transfer)",
-            "delegation-3": "no pay and no transfer or pay.transfer",
+        "iteration": range(10),
+        "property": {
+            "live": {"expected": "all", "query": -nonlive(Sale)},
+            "safe": {"expected": "all", "query": -unsafe(Sale)},
+            "desired-end": {
+                "expected": "all",
+                "query": "rescindAck or reject or transfer and (refund or deliver)",
+            },
+            "create-detach-discharge": {
+                "expected": "some",
+                "query": "offer . accept and offer . transfer and accept . deliver and transfer . deliver",
+            },
+            "flexibility": {
+                "expected": "some",
+                "query": "accept . deliver and deliver . transfer",
+            },
+            "delegation-guarantee": {
+                "expected": "all",
+                "query": "no pay and no transfer or pay . transfer",
+            },
+            "concede": {
+                "expected": "all",
+                "query": "pay . Buyer:rescind or (no rescind and no rescindAck) or rescind . rescindAck",
+            },
+            "late-action": {
+                "expected": "all",
+                "query": "no accept or no rescind or accept . Buyer:rescind",
+            },
+            "compensation": {
+                "expected": "all",
+                "query": "(no transfer or deliver or refund) and (no refund or transfer) and (no refund or no deliver)",
+            },
+            "complementary": {"expected": "none", "query": "reject and deliver"},
+            "deadwood": {"expected": "none", "query": "deadwood"},
         },
-        "iteration": range(1),
     },
 )
 
+properties = experiment.parameters["property"]
+
 
 @experiment.action
-def check_mambo(iteration=None, query=None, **kwargs):
-    q = experiment.parameters["query"][query]
-    print(f"{query}: {precedence.parse(q, semantics=QuerySemantics())}")
-    result = list(match_paths(Sale, q, residuate=True, prune=True, incremental=True))
-    nresult = list(
-        match_paths(Sale, f"no ({q})", residuate=True, prune=True, incremental=True)
+def check_mambo(iteration=None, property=None, **kwargs):
+    if property == "deadwood":
+        dead = mambo.deadwood(Sale)
+        return {"satisfied": not dead}
+    criteria = properties[property]["expected"]
+    q = precedence.parse(properties[property]["query"], semantics=QuerySemantics())
+
+    if criteria == "all":
+        # negate query to prove that none match the negation
+        q = -q
+
+    result = list(
+        match_paths(
+            Sale,
+            q,
+            residuate=True,
+            prune=True,
+            incremental=True,
+            safe=True,
+            # verbose=True,
+            # max_only=True,
+        )
     )
-    if nresult:
-        print("\n".join([str(p.events) for p in nresult]))
-        print()
-    return {"result": (len(result), len(nresult))}
+
+    if criteria == "all":
+        return {"satisfied": not result}
+    if criteria == "some":
+        return {"satisfied": bool(result)}
+    if criteria == "none":
+        return {"satisfied": not bool(result)}
 
 
 @experiment.postprocess
 def postprocess(results):
     results["duration"] = results["duration"] * 1000
     stats = (
-        results.groupby(["query", "result"])
+        results.groupby(["property", "satisfied"])
         .agg({"duration": ["mean", "std", "min", "max", "count"]})
         .round(3)
     )
     print(stats)
-    # return stats
+    return stats
 
 
 if __name__ == "__main__":
